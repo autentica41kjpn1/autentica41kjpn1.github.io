48fd7f0c5eac62222f1462c3165d41519307c0d5
autentica41kjpn1 <autentica41kjpn1@gmail.com>
2023-03-17 00:21:46 -0600

    Cambio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyA_dR8_KhE15gLjtzjyHyWzzXoIWr9553s",
+  authDomain: "autentica41kjpn2.firebaseapp.com",
+  projectId: "autentica41kjpn2",
+  storageBucket: "autentica41kjpn2.appspot.com",
+  messagingSenderId: "4356880837",
+  appId: "1:4356880837:web:25394f1c7e885a74c273ac",
+  measurementId: "G-B43MWDWFMK"
+});
